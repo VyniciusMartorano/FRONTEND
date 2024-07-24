ab05675afed49ed44bec734787af0ea5487f9afe
VyniciusMartorano <vyniciussalusto@gmail.com>
2024-07-23 22:11:00 -0300

    CARD cadastro

--- src/pages/cadastro/Cadastro.vue
+++ src/pages/cadastro/Cadastro.vue
@@ -1,14 +1,39 @@
 <template>
-    <div>
-<h1>cadastro </h1>
+    <div class="flex justify-center p-4">
+        <Card class="w-8/12">
+            <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+            <CardContent>
+                Card Content
+            </CardContent>
+            <CardFooter>
+                Card Footer
+            </CardFooter>
+        </Card>
     </div>
 </template>
 
 <script>
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
-export default {
-name: 'Cadastro', 
+
+export default {name: 'Cadastro', 
 components: {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
 },
 methods: {
 
